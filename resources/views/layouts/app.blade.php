75ebc4607459f7abff22651dca178d705b65da30
Gerasimov1991 <svt.triya1991@gmail.com>
2020-09-24 18:40:53 +1000

    version2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
         <nav class="navbar navbar-expand-md navbar-light shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}" style="color: white;">
-                    Welcome To Here
+                <img src="{{ asset('img/logo.png') }}" style="height:50px;" alt="" srcset="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
