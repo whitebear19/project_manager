d6172471dec331c47476bd6333b61281292bd669
Gerasimov1991 <svt.triya1991@gmail.com>
2020-09-24 19:08:32 +1000

    version2

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'role' => '1',
             'plan' => '0',
-            'paid' => '0',
+            'paid' => '1',
             'password' => Hash::make($data['password']),
         ]);
     }
